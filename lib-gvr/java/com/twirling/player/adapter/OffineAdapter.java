8e74087c81d4dd3b176335052b503ffd5a734dc2
谢秋鹏 <xieqiupeng@gmail.com>
2017-02-25 15:28:23 +0800

    update widget icon

--- lib-gvr/java/com/twirling/player/adapter/OffineAdapter.java
+++ lib-gvr/java/com/twirling/player/adapter/OffineAdapter.java
@@ -3,12 +3,18 @@ package com.twirling.player.adapter;
 import android.content.Context;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
+import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.afollestad.materialdialogs.DialogAction;
+import com.afollestad.materialdialogs.MaterialDialog;
+import com.jakewharton.rxbinding2.view.RxView;
 import com.twirling.lib_cobb.util.FileUtil;
+import com.twirling.lib_cobb.widget.WidgetIcon;
 import com.twirling.player.Constants;
 import com.twirling.player.R;
 import com.twirling.player.activity.VRPlayerActivity;
@@ -18,6 +24,10 @@ import com.twirling.player.widget.BindingViewHolder;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.functions.Consumer;
 
 /**
  * Target: 适配本地列表单项
@@ -50,13 +60,42 @@ public class OffineAdapter extends RecyclerView.Adapter<BindingViewHolder> {
 	}
 
 	public class Presenter {
-		public void onIvDeleteClick(View view, OfflineModel item) {
-			FileUtil.Delete.deleteByPath(Constants.PATH_MOVIES + item.getName());
-			models.remove(item.getPosition());
-//			DiffUtil.DiffResult result = DiffUtil.calculateDiff(new DiffCallBack<>(oldModels, models), true);
-//			result.dispatchUpdatesTo(OffineAdapter.this);
-//			setModels(models);
-//			notifyItemRemoved(position);
+		public void onIvDeleteClick(final View view, final OfflineModel item) {
+			RxView.clicks(view)
+					.throttleFirst(2, TimeUnit.SECONDS)
+					.observeOn(AndroidSchedulers.mainThread())
+					.subscribeOn(AndroidSchedulers.mainThread())
+					.subscribe(new Consumer<Object>() {
+						@Override
+						public void accept(Object o) throws Exception {
+							// 删除本地文件
+							new MaterialDialog.Builder(view.getContext())
+									.onPositive(new MaterialDialog.SingleButtonCallback() {
+										@Override
+										public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+											FileUtil.Delete.deleteByPath(Constants.PATH_MOVIES + item.getName());
+											//
+											models.remove(item.getPosition());
+											//
+//											DiffUtil.DiffResult result = DiffUtil.calculateDiff(new DiffCallBack<>(oldModels, models), true);
+//											result.dispatchUpdatesTo(OffineAdapter.this);
+//											setModels(models);
+//											notifyItemRemoved(position);
+											notifyDataSetChanged();
+										}
+									})
+									.content("确定删除 " + item.getName() + " 吗")
+									.icon(WidgetIcon.getTrashIcon(view.getContext()))
+									.show();
+						}
+					}, new Consumer<Throwable>() {
+						@Override
+						public void accept(Throwable throwable) throws Exception {
+							Log.e(getClass() + "", throwable.toString());
+						}
+					});
+
+
 			notifyDataSetChanged();
 		}
 
